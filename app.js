2d99011f7923486f599a0ad96650a6c5358f2035
Erdem-Atik <erdematik8@gmail.com>
2022-01-01 22:10:14 +0300

    adding comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const selectionButtons = document.querySelectorAll("[data-selection]");
 const finalColumn = document.querySelector("[data-final-column]");
 const computerScoreSpan = document.querySelector("[data-computer-score]");
 const yourScoreSpan = document.querySelector("[data-your-score]");
+
 const SELECTIONS = [
   {
     name: "rock",
@@ -20,7 +21,10 @@ const SELECTIONS = [
   },
 ];
 
+// determine which button select
 selectionButtons.forEach((selectionButton) => {
+  //  iterable DOM elements!
+  // node list
   selectionButton.addEventListener("click", (e) => {
     const selectionName = selectionButton.dataset.selection; // dataset read-only property of the HTMLElement interface provides read/write access to custom data attributes (data-*) on elements.selection;
     const selection = SELECTIONS.find(
@@ -30,8 +34,9 @@ selectionButtons.forEach((selectionButton) => {
   });
 });
 
+//determine whether you or computer win
 function makeSelection(selection) {
-  // function dec. is hosted
+  // function dec. is hoisted
   console.log("player selection:", selection);
 
   const computerSelection = randomSelection();
@@ -46,22 +51,26 @@ function makeSelection(selection) {
   if (computerWinner) incrementScore(computerScoreSpan);
 }
 
+// indicate score
 function incrementScore(scoreSpan) {
   scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
 }
 
+// adding winner element
 function addSelectionResult(selection, winner) {
-  const div = document.createElement("div");
+  const div = document.createElement("div"); //
   div.innerText = selection.emoji;
   div.classList.add("result-selection");
   if (winner) div.classList.add("winner");
   finalColumn.after(div);
 }
 
+// indicate winner
 function isWinner(selection, opponentSelection) {
   return selection.beats === opponentSelection.name; // check who winner as regards computer selection or your selection object's property
 }
 
+// determine computer select which one
 function randomSelection() {
   const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
   return SELECTIONS[randomIndex];
